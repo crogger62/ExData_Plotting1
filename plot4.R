e22bd91851864969fee68c5dc892a65c70f1ad9d
Craig Lewis <crog62@gmail.com>
2015-05-08 12:05:42 -0600

    Fixed plot3/added plot4
    Plot3 corrected and plot4 committed.

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-plot3 <- function() {
+plot4 <- function() {
     
   ## reads file in dates are factors; call out na.strings as question marks
   epc<-read.delim("household_power_consumption.txt", sep=";", na.strings=c("?"))
@@ -13,20 +13,32 @@ plot3 <- function() {
   
   # Beginning of plot
   
-  png("plot3.png")                            # png device
+  png("plot4.png")                            # png device
   library(lubridate)                          # need this for dates
                                               # create a single date+time field
   dt<-paste(dateRange$Date, dateRange$Time)
   plot_dt<-dmy_hms(dt)                        # and convert using lubridate to a format to plot
                                               # convert the Global active power factors to numbers
   gap<-as.numeric(dateRange$Global_active_power) 
-                                              # and plot!
+
+# Setup four quadrant graph
+  par(mfrow=c(2,2))
+
+# Top Left
+  plot(plot_dt,gap,ylab="Global Active Power",type="o",xlab="",pch=NA)  
+
+# Top Right
+  plot(plot_dt,dateRange$Voltage,pch=NA, type="o",xlab="datetime",ylab="Voltage")
+
+# bottom left
   plot(plot_dt, dateRange$Sub_metering_1,type="o",col="black",pch=NA,ylab="Energy sub metering",xlab="")
   lines(plot_dt, dateRange$Sub_metering_2,type="o",col="red",pch=NA)
   lines(plot_dt, dateRange$Sub_metering_3,type="o", col="blue",pch=NA)
-  
   legend("topright", lty=1, col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
- 
+  
+# bottom right
+  plot(plot_dt,dateRange$Global_reactive_power,type="o",pch=NA,xlab="datetime",ylab="Global_reactive_power")
+
   dev.off()                                   # close device!
   
 }
