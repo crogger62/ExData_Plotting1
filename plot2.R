273b0b0554cf78769b39e54ac22330e6a48d19f6
Craig Lewis <crog62@gmail.com>
2015-05-07 20:54:08 -0600

    Commit for Plot 2
    plot2.R and plot2.png for assignment

---
+++ plot2.R
@@ -0,0 +1,28 @@
+plot2 <- function() {
+    
+  ## reads file in dates are factors; call out na.strings as question marks
+  epc<-read.delim("household_power_consumption.txt", sep=";", na.strings=c("?"))
+  epc$Dates<-as.Date(epc$Date,"%d/%m/%Y")       # add a date column
+  
+  date1<-as.Date("01/02/2007","%d/%m/%Y")
+  date2<-as.Date("02/02/2007","%d/%m/%Y")
+    
+  dates1 <- c(date1,date2)                     # dates we are interested in
+                                               # subset dates
+  dateRange<-subset(epc,epc$Dates %in% dates1)
+  
+  # Beginning of plot
+  
+  png("plot2.png")                            # png device
+  library(lubridate)                          # need this for dates
+                                              # create a single date+time field
+  dt<-paste(dateRange$Date, dateRange$Time)
+  plot_dt<-dmy_hms(dt)                        # and convert using lubridate to a format to plot
+                                              # convert the Global active power factors to numbers
+  gap<-as.numeric(dateRange$Global_active_power) 
+                                              # and plot!
+  plot(plot_dt,gap,ylab="Global Active Power (kilowats)",type="o",xlab="",pch=NA)  
+  
+  dev.off()                                   # close device!
+  
+}
